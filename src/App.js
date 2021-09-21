fdad46b9c2976e1328e7e73a6f089d1af5c6856b
vishwajeet <singhvishwajeet1998@gmail.com>
2021-09-21 12:45:11 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Menu from "./components/menu/Menu";
+import Navbar from "./components/navbar/Navbar";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Works from "./components/works/Works";
+import "./app.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/works/Works.js
@@ -0,0 +1,7 @@
+import "./works.scss";
+
+const Works = () => {
+  return <div className="works"></div>;
+};
+
+export default Works;
